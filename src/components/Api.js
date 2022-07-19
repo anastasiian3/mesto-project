86e078cec425c7cfe2824c5baf6b7bda0aef96b0
Nikolay Brovkin <bronik004@yandex.ru>
2022-07-19 15:51:18 +0300

    fix: Заглавные буквы в названиях классов

---
+++ src/components/Api.js
@@ -0,0 +1,90 @@
+export default class Api {
+  constructor({ url, headers }) {
+    // тело конструктора
+    this._url = url;
+    this._headers = headers;
+  }
+
+  _checkResponse(responce) {
+    return responce.ok ? responce.json() : Promise.reject(responce);
+  }
+
+  //методы работы с API
+
+  //загрузка всех карточек с сервера
+  getCards() {
+    return fetch(`${this._url}/cards`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  //получение информации о пользователе
+  getUser() {
+    return fetch(`${this._url}/users/me`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  getAllInfo() {
+    return Promise.all([this.getCards(), this.getUser()]);
+  }
+
+  //редактирование информации о пользователе
+  editProfile(data) {
+    return fetch(`${this._url}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then(this._checkResponse);
+  }
+
+  //добавление новой карточки
+  addCard(data) {
+    return fetch(`${this._url}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then(this._checkResponse);
+  }
+
+  //удаление карточки
+  removeCard(dataId) {
+    return fetch(`${this._url}/cards/${dataId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  //изменение аватара
+  editUserAvatar(data) {
+    return fetch(`${this._url}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then(this._checkResponse);
+  }
+
+  addLike(dataId) {
+    return fetch(`${this._url}/cards/likes/${dataId}`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  // метод удаления лайка
+  removeLike(dataId) {
+    return fetch(`${this._url}/cards/likes/${dataId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  // изменение состояния лайка
+  // changeLikeStatus(dataId, isLiked) {
+  //   return fetch(`${this._url}/cards/likes/${dataId}`, {
+  //     method: isLiked ? "DELETE" : "PUT",
+  //     headers: this._headers,
+  //   }).then(this._checkResponse);
+  // }
+  // метод добавления лайка
+}
